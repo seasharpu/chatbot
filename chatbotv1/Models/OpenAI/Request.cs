f2902bb4cbd765a0fe4a0f0d5b1073c122c8b9fe
Maz <maz.grasl@gmail.com>
2024-05-01 21:34:40 +0200

    Fixed compilation

--- chatbotv1/Models/OpenAI/Request.cs
+++ chatbotv1/Models/OpenAI/Request.cs
@@ -4,7 +4,7 @@ namespace chatbotv1.Models.OpenAI {
     public class OpenAIRequest
     {
         public string Model { get; set; } = "gpt-3.5-turbo";
-        public History Messages { get; set; } = new History();
+        public History Messages { get; set; } = default!;
         //public float? Frequency_penalty { get; set; } = 0;
     }
 }
